5a3b008b7ebde6721075a9d41fa4080bd22a6def
Ammon Casey <ammon@caseyohana.com>
2014-05-19 15:30:44 -1000

    added centos-6.5-x86_64-minimun.json template

---
+++ packer/scripts/centos/slimmer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -eux
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
---
+++ packer/scripts/centos/utils.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=Extra Packages for Enterprise Linux 6 - \$basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=\$basearch
+failovermethod=priority
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y install gcc gcc-c++ make kernel-devel-`uname -r` perl nfs-utils wget
